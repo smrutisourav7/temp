929bf96c325c43d6f30bf92d7f5dcd80a024ce4a
Smruti Sourav Panda <smrutisouravpanda@gmail.com>
2023-07-29 13:06:44 +0530

    Some minor changes

--- src/pages/recipe.jsx
+++ src/pages/recipe.jsx
@@ -1,29 +1,27 @@
 // import logo from './logo.svg';
-import { useState, useEffect } from 'react';
-import './recipe.css';
+import { useState, useEffect } from "react";
+import "./recipe.css";
 // import "./key";
-import axios from 'axios';
+import axios from "axios";
 // import './RecipeTitle';
 // import { Key } from 'react';
-import RecipeTitle from './RecipeTitle';
-// import Footer from '../components/footer';
-import Navbar from '../components/navbar';
+import RecipeTitle from "./RecipeTitle";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
 
 function Recipe() {
-  const [query, setquery] = useState("mango");
+  const [query, setquery] = useState("pizza");
   const [recipes, setrecipes] = useState([]);
   // const [healthLabels, sethealthLabels] = useState("")
   // const [search, setSearch] = useState("")
 
+  const YOUR_APP_ID = "34f668d8";
+  const YOUR_APP_KEY = "caeec21cecebc0681a9968eff0a06f28";
 
-  const YOUR_APP_ID="5c63639d";
-  const YOUR_APP_KEY="4fdc677fc8be71a3fc9c34757ca65b32";
+  var url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
 
-
-  var url=`https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
-  
-  async function getRecipes(){
-    var result=await axios.get(url);
+  async function getRecipes() {
+    var result = await axios.get(url);
     setrecipes(result.data.hits);
     console.log(result.data);
   }
@@ -33,31 +31,57 @@ function Recipe() {
   const onSubmit = (e) => {
     e.preventDefault();
     getRecipes();
-  }
+  };
 
   return (
     <>
-    <Navbar />
-    <div className="recipe_App">
-      <h1  style={{fontSize: "52px", textAlign:"center", fontFamily: "Chronicle Deck,Times,serif"}}>Explore Our Meal Delivery Menus</h1>
-      <form className="app__searchForm" onSubmit={onSubmit}>
-        <input style={{width:"300px", border:"1px solid rgb(26, 26, 114)", height:"40px"}} type="text" className="app__input" placeholder="Enter Ingredients" value={query} onChange={(e) => setquery(e.target.value)} />
-        <input style={{width:"120px", height:"45px"}} className="app__submit"type="submit" value="Search" />
-        
-        {/* <select className="app__healthlabels">
+      <Navbar />
+      <div className="recipe_App">
+        <h1
+          style={{
+            fontSize: "52px",
+            textAlign: "center",
+            fontFamily: "Chronicle Deck,Times,serif",
+          }}
+        >
+          Explore Our Meal Delivery Menus
+        </h1>
+        <form className="app__searchForm" onSubmit={onSubmit}>
+          <input
+            style={{
+              width: "300px",
+              border: "1px solid rgb(26, 26, 114)",
+              height: "40px",
+            }}
+            type="text"
+            className="app__input"
+            placeholder="Enter Ingredients"
+            value={query}
+            onChange={(e) => setquery(e.target.value)}
+          />
+          <input
+            style={{ width: "120px", height: "45px" }}
+            className="app__submit"
+            type="submit"
+            value="Search"
+          />
+
+          {/* <select className="app__healthlabels">
           <option onClick={() => sethealthLabels("vegeterian")}>Vegeterian</option>
         </select> */}
-      </form>
-      <p style={{marginTop: "25px"}}>Choose from an ever-changing mix of meat, fish, and health-conscious offerings.</p>
+        </form>
+        <p style={{ marginTop: "25px" }}>
+          Choose from an ever-changing mix of meat, fish, and health-conscious
+          offerings.
+        </p>
 
-      <div className="app__recipes">
-        {recipes.map(recipe => {
-          return <RecipeTitle recipe={recipe}/>
-        })}
-
+        <div className="app__recipes">
+          {recipes.map((recipe) => {
+            return <RecipeTitle recipe={recipe} />;
+          })}
+        </div>
       </div>
-    </div>
-    {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
